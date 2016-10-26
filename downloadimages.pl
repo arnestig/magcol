a5100df1c761ae74cde693a763ce6b0a7f9593a0
Tobias Eliasson <arnestig@gmail.com>
2016-10-26 15:35:13 +0200

    - Updated perl scripts for parsing and downloading images

--- downloadimages.pl
+++ downloadimages.pl
@@ -3,35 +3,77 @@
 use warnings;
 use strict;
 use Data::Dumper;
-use DBI;
-use File::Fetch;
+use LWP::Simple;
+use JSON;
+use threads;
 
-our %IMAGES;
+our @images;
 $| = 1;
-
-my $dbh = DBI->connect("DBI:Pg:dbname=magcol;host=localhost", "dbamagcol", "magcol", {'RaiseError' => 1});
 
 sub download_image
 {
-    my($url,$path) = @_;
-    my $ff = File::Fetch->new( uri => $url );
-    $ff->fetch( to => "images/$path" ) or die $ff->error();
+    my($multiverseid,$name) = @_;
+    my $url = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=$multiverseid&type=card";
+    print "Downloading $name\n";
+    getstore( $url, "images/$name" );
 }
 
 sub fetch_img_refs
 {
-    $dbh->{ AutoCommit } = 1;
-    # select our stored procedure for hosts to scan
-    my $sth = $dbh->prepare( 'SELECT card_image_ref,card_expansion FROM cards;' );
-    $sth->execute();
-
-    # get all objects to scan
-    my $hash_dump = $sth->fetchall_hashref( 'card_image_ref' );
-    return $hash_dump;
 }
 
-my $images = fetch_img_refs();
-foreach ( keys %{ $images } ) {
-    print "Download $_ to $images->{ $_ }{ 'card_expansion' }\n";
-    download_image( $_, $images->{ $_ }{ 'card_expansion' } );
+if ( ! -e "ISD.json" ) {
+    print "Downloading json database\n";
+    my $ff = File::Fetch->new( uri => "http://mtgjson.com/json/ISD.json" );
+    $ff->fetch( to => "." ) or die $ff->error();
 }
+
+open( JSON_DB, "<ISD.json" ) or die( "Error: $!" );
+my $json = JSON->new->allow_nonref;
+my $json_ref = $json->decode( <JSON_DB> );
+my %json_hash = %{ $json_ref };
+close( JSON_DB );
+foreach my $card ( @{ $json_ref->{ cards } } ) {
+    my $name = $card->{ name };
+    $name =~ s/[^a-zA-Z ]//g;
+    $name =~ s/ /_/g;
+    my $expansion = "ISD";
+    if ( defined $card->{ multiverseid } ) {
+        my $mid = $card->{ multiverseid };
+        my %image;
+        $image{ mid } = $mid;
+        $image{ name } = "$name\_$expansion\_$mid.jpg";
+        push( @images, \%image );
+    }
+}
+
+foreach my $image ( @images ) {
+    if ( ! -e "images/$image->{ name }" ) {
+        download_image( $image->{ mid }, $image->{ name } );
+        # now we should do all three calculations on the downloaded image.
+        # Calling genimagehash with different arguments
+        # also this is single thread right now, code below works but FF is not thread safe. Should give curl a try instead
+    }
+}
+
+#while ( @images ) {
+    #my @images_to_download;
+    #my @thread_pool;
+#
+    #if ( $#images >= 19 ) {
+        #@images_to_download = splice( @images, -20 );
+    #} else {
+        #@images_to_download = splice( @images, -$#images-1 );
+    #}
+#
+    #foreach my $image ( @images_to_download ) {
+        #if ( ! -e "images/$image->{ name }" ) {
+            #my $thread = threads->new(\&download_image, $image->{ mid }, $image->{ name } );
+            #push( @thread_pool, $thread );
+        #}
+    #}
+#
+    #foreach ( @thread_pool ) {
+        #$_->join();
+    #}
+#}
